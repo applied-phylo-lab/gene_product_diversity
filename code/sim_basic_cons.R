97883dbada4ca4f4c63414fb00e684a1d1dd5124
Rex Jiang <61212763+RexJiangEvoBio@users.noreply.github.com>
2024-03-25 15:39:54 -0700

    Add files via upload
    Adding code for computing conservation of modification sites (probability a modification site is shared by two lineages after a given amount of time)

---
+++ code/sim_basic_cons.R
@@ -0,0 +1,227 @@
+# Get relationship between time and probability of sharing (conservation)
+
+library(expm)
+library(ggplot2)
+
+# Calculate normalized cis-genotypic value
+# Input: genotype (a vector of 0 and 1), effect sizes of effector allele at all loci (a vector)
+cis.gv <- function(gt,effect){
+	v=sum(gt*effect)
+	vmax=sum(effect)
+	return(v/vmax)
+}
+
+# Calculate modification rate beta
+# beta as a linear function of cis-genotypic value
+# Input: normalized cis-genotypic value (a number, calculated using cis.gv()), trans- genotypic value, shape parameter characterizing relationship between cis- loci and enzyme-substrate affinity (a number), background/non-specific modification activity (a number)
+beta.calc <- function(cis,trans,C,epsilon=0){
+	beta=trans*(C*cis+epsilon)
+	return(beta)
+}
+
+# Genotype-phenotype mapping (2 isoforms)
+# Input: genotypic value of overall expression level, rate of modification (calculated using beta.calc()), decay rates (numbers)
+g2p <- function(alpha,beta,gamma_0,gamma_1){
+	P_0=alpha/(beta+gamma_0)
+	P_1=alpha*beta/(gamma_1*(beta+gamma_0))
+	return(c(P_0,P_1))
+}
+
+# Calculate fitness w/ respect to an isoform under stabilizing selection
+# Input: isoform abundance (a number), optimum (a number), SD of fitness function
+# Input values are not log-transformed and should be log-transformed by the function
+fitness.stab <- function(z,opt,sig){
+	d=log(z/opt) # Convert to log scale
+	if(sig>0){
+		w=dnorm(d,mean=0,sd=sig)/dnorm(0,mean=0,sd=sig)
+	}else{
+		w=1
+	}
+	return(w)
+}
+
+# Calculate fitness w/ respect to a deleterious isoform
+# Input: isoform abundance, shape parameter (a positive number)
+fitness.del <- function(z,par){
+	w=exp(-par*z)
+	return(w)
+}
+
+# Overall fitness as a product of fitness w/ respect to each isoform
+# Input: isoform abundances (a vector), selection parameter (a list; vector-->stabilizing, number-->deleterious, NULL=neutral/equivalent to I_0), whether each modified isoform is functionally equivalent to I_0 (a vector, 1 means equivalent to I_0)
+fitness.prod <- function(z,selection,eq=NULL){
+	w=rep(0,length(z))
+	type=lengths(selection) # Type of selection, which corresponds to number of selection parameters
+	if(length(eq)>0){
+		z[1]=z[1]+sum(z[which(eq==1)])
+	}
+	for(i in 1:length(z)){
+		if(type[i]==2){
+			w[i]=fitness.stab(z[i],selection[[i]][1],selection[[i]][2])
+		}else{
+			if(type[i]==1){
+				w[i]=fitness.del(z[i],selection[[i]])
+			}else{
+				w[i]=1
+			}
+		}
+	}
+	return(prod(w))
+}
+
+# Fixation probability given ancestral and mutant fitness
+fix.prob <- function(wa,wm,Ne){
+	if(wa>0){
+		s=(wm/wa)-1 # Coefficient of selection
+		if(s==0){
+			p=1/(2*Ne)
+		}else{
+			p=(1-exp(-2*s))/(1-exp(-4*Ne*s))
+		}
+	}else{ # The ancestral fitness is close to 0 (close enough to be recognized as zero by R)
+		if(wm==0){ # Both ancestral and mutant fitness are close to 0
+			p=1/(2*Ne) # The mutation is considered neutral
+		}else{ # Ancestral fitness is close to 0 while mutant fitness isn't
+			p=1 # The mutation is considered strongly beneficial
+		}
+	}
+	return(p)
+}
+
+# Obtain transition matrix given other parameters
+# Simplest version: 2 isoforms, constant transition matrix (no trans- substitutions, no environmental change), all cis- loci have equal effect size (i.e., no. of states = no. of loci+1) and mutational spectrum
+# Input: number of cis- loci, mutation rates (a vector, elements being u01 and u10, respectively; 2*Ne*u should not exceed 1), selection parameter (a list), effective population size, other parameters (alpha,trans,C,gamma_0,gamma_1,epsilon,eq)
+tm <- function(nloci,u,selection,Ne,alpha,trans,C,epsilon=0,gamma_0,gamma_1,eq=NULL){
+	mat=matrix(0,nrow=nloci+1,ncol=nloci+1)
+	for(i in 1:nrow(mat)){
+		v1=(i-1)/nloci
+		beta1=beta.calc(v1,trans,C,epsilon)
+		z1=g2p(alpha,beta1,gamma_0,gamma_1)
+		w1=fitness.prod(z1,selection,eq)
+		for(j in 1:ncol(mat)){
+			if(abs(i-j)==1){ # Consider adjescent states only
+				v2=(j-1)/nloci
+				beta2=beta.calc(v2,trans,C,epsilon)
+				z2=g2p(alpha,beta2,gamma_0,gamma_1)
+				w2=fitness.prod(z2,selection,eq)
+				fp=fix.prob(w1,w2,Ne)
+				if(i<j){
+					uij=2*Ne*(nloci-(i-1))*u[1]
+				}else{
+					uij=2*Ne*(i-1)*u[2]
+				}
+				mat[i,j]=uij*fp
+			}
+		}
+	}
+	for(i in 1:nrow(mat)){
+		mat[i,i]=1-sum(mat[i,])
+	}
+	return(mat)
+}
+
+T.all=c(1e5,1e6,1e7,1e8) # Time (may not be enough to reach stationary distribution, final dependent on initial state)
+
+# Test run
+out=rep(0,length(T.all))
+Ne=1e4
+alpha=1
+nloci=5
+trans=1
+u=c(1e-9,1e-9)
+gamma_0=1;gamma_1=1
+opt=alpha/gamma_0 # Make optimal P_0 the value reached in the absence of mis-splicing
+par=list(c(opt,10),1e-3)
+mat=tm(nloci,u,par,Ne,alpha,trans,C,espsilon,gamma_0,gamma_1)
+start=rep(0,nloci+1);start[length(start)]=1
+for(t in 1:length(T.all)){
+	distr=start%*%(mat %^% T.all[t])
+	out[t]=1-distr[1]
+}
+
+Ne.all=c(1e2,1e3,1e4,1e5)
+alpha.all=exp(0:5)
+nloci.all=1:10
+u.all=1e-9*rbind(c(1,1),c(1.5,0.5),c(0.5,1.5))
+trans.all=(1:10)/5
+C=1
+gamma_0=1;gamma_1=1
+sig=10 # Width of fitness function for P_0
+lambda=1e-3 # Strength of selection on P_1
+
+comb.all=rep(0,6)
+for(Ne in Ne.all){
+	for(alpha in alpha.all){
+		for(nloci in nloci.all){
+			for(trans in trans.all){
+				for(i in 1:nrow(u.all)){
+					row=c(Ne,alpha,nloci,trans,u.all[i,])
+					comb.all=rbind(comb.all,row)
+				}
+			}
+		}
+	}
+}
+comb.all=comb.all[2:nrow(comb.all),]
+rownames(comb.all)=NULL
+
+# Get expected mean phenotype when I_0 is functional and I_1 is deleterious
+# Output data matrix (columns: mean normalized cis- genotypic value, isoform abundances and modification frequency based on the mean gv)
+out=matrix(0,nrow=nrow(comb.all),ncol=length(T.all))
+for(i in 1:nrow(comb.all)){
+	Ne=comb.all[i,1]
+	alpha=comb.all[i,2]
+	nloci=comb.all[i,3]
+	trans=comb.all[i,4]
+	u=comb.all[i,5:6]
+	opt=alpha/gamma_0 # Make optimal P_0 the value reached in the absence of mis-splicing
+	par=list(c(opt,sig),lambda)
+	mat=tm(nloci,u,par,Ne,alpha,trans,C,espsilon,gamma_0,gamma_1)
+	start=rep(0,nloci+1);start[length(start)]=1
+	for(t in 1:length(T.all)){
+		distr=start%*%(mat %^% T.all[t])
+		if(distr[1]>1){ # If the probability is >1, set it to 1
+			distr[1]=1
+		}
+		out[i,t]=(1-distr[1])^2 # Probability that cis-genotypic value is >0 in two lineages
+	}
+}
+
+out=data.frame(comb.all,out)
+colnames(out)=c("Ne","alpha","l","Q","u01","u10","prob1","prob2","prob3","prob4")
+out$alpha=log(out$alpha)
+write.table(out,file="out_basic_cons.txt",sep="\t")
+
+
+# Plot a subset
+d<-read.table("out_basic_cons.txt",sep="\t",header=TRUE)
+
+# Check effect of Ne, nloci, and mutational bias
+sub=which((d$alpha==0)&(d$Q==1)&(d$u01==d$u10)) # Rows with mutational bias
+#sub=which((d$alpha==0)&(d$Q==1)&(d$u01>d$u10)) # Rows with mutational bias towards the effector allele
+#sub=which((d$alpha==0)&(d$Q==1)&(d$u01<d$u10)) # Rows with mutational bias towards the null allele
+dsub=d[sub,]
+dsub$l=factor(dsub$l,levels=sort(unique(dsub$l)),ordered=TRUE)
+g<-ggplot(dsub,aes(x=log10(Ne),y=prob4,colour=l))
+g=g+geom_point()+geom_line()+scale_color_brewer(palette="Paired")
+g=g+theme_classic()
+g=g+xlab(expression(paste("Lo",g[10],N[e])))+ylab("Probability of sharing")
+lt=expression(paste("Number of ",italic(cis),"- loci"))
+g=g+labs(color=lt)
+g=g+ylim(c(0,1))
+g=g+theme(axis.text=element_text(size=12),axis.title=element_text(size=15),legend.text=element_text(size=15),legend.title=element_text(size=15),legend.key.size=unit(0.7,'cm')) # Adjust font size of axis labels, axis text, and legend
+ggsave("plot_basic_cons_1e8.pdf",plot=g,width=6,height=5)
+#ggsave("plot_basic_cons_1e8_mb1.pdf",plot=g,width=6,height=5)
+#ggsave("plot_basic_cons_1e8_mb2.pdf",plot=g,width=6,height=5)
+g<-ggplot(dsub,aes(x=log10(Ne),y=prob3,colour=l))
+g=g+geom_point()+geom_line()+scale_color_brewer(palette="Paired")
+g=g+theme_classic()
+g=g+xlab(expression(paste("Lo",g[10],N[e])))+ylab("Probability of sharing")
+lt=expression(paste("Number of ",italic(cis),"- loci"))
+g=g+labs(color=lt)
+g=g+ylim(c(0,1))
+g=g+theme(axis.text=element_text(size=12),axis.title=element_text(size=15),legend.text=element_text(size=15),legend.title=element_text(size=15),legend.key.size=unit(0.7,'cm')) # Adjust font size of axis labels, axis text, and legend
+ggsave("plot_basic_cons_1e7.pdf",plot=g,width=6,height=5)
+#ggsave("plot_basic_cons_1e7_mb1.pdf",plot=g,width=6,height=5)
+#ggsave("plot_basic_cons_1e7_mb2.pdf",plot=g,width=6,height=5)
+
